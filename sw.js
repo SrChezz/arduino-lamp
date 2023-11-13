3a59878416970daa9caaeced99e9694541a499eb
Chezzby <itzchezvil17@gmail.com>
2023-11-13 03:12:35 -0500

    try 7

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open('my-cache').then(function (cache) {
             return cache.addAll([
+                '/',
                 'index.html',
                 'style.css',
                 'script.js',
