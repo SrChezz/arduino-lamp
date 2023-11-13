c484ca8e7da9e0f35c2064b7998404091ed8e712
Chezzby <itzchezvil17@gmail.com>
2023-11-13 01:40:28 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const root = document.querySelector(':root');
+const powerButton = document.getElementById("power");
+
+powerButton.addEventListener("click", (e) => {
+
+    rs = getComputedStyle(root);
+    console.log(rs.getPropertyValue('--current-dark'));
+
+    let currentSun = rs.getPropertyValue('--current-sun');
+
+    let switchSound = new Audio("media/lamp.mp3");
+    switchSound.volume = 0.2
+    switchSound.play();
+
+
+
+    if (currentSun == '#FFC436') {
+        // root.style.setProperty('--current-dark', 'var(--day-dark)');
+        // root.style.setProperty('--current-light', 'var(--day-light)');
+        // root.style.setProperty('--current-color', 'var(--day-color)');
+        root.style.setProperty('--current-sun', 'var(--day-sun)');
+    } else {
+        // root.style.setProperty('--current-dark', 'var(--night-dark)');
+        // root.style.setProperty('--current-light', 'var(--night-light)');
+        // root.style.setProperty('--current-color', 'var(--night-color)');
+        root.style.setProperty('--current-sun', 'var(--night-sun)');
+    }
+});
